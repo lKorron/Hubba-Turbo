904f19fcb25d6f46bf8a4ee8105069526ff423f2
Gregory <lkorron@yandex.ru>
2020-07-27 12:24:34 +0300

    Fix pause bug

--- Hubba-Turbo New/Assets/Scripts/LevelChoice.cs
+++ Hubba-Turbo New/Assets/Scripts/LevelChoice.cs
@@ -8,16 +8,19 @@ public class LevelChoice : MonoBehaviour
     // load main menu
     public void LoadMenu()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(0);
     }
     // load level by index
     public void LoadLevel(int level)
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(level);
     }
     // reload current level
     public void ReloadLevel()
     {
+        Time.timeScale = 1f;
         Scene scene = SceneManager.GetActiveScene();
         int index = scene.buildIndex;
         SceneManager.LoadScene(index);
@@ -25,6 +28,7 @@ public class LevelChoice : MonoBehaviour
 
     public void NextLevel()
     {
+        Time.timeScale = 1f;
         Scene scene = SceneManager.GetActiveScene();
         int index = scene.buildIndex;
         SceneManager.LoadScene(index + 1);
