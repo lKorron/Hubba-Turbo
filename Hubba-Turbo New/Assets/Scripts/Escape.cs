d234ca16f48ee3887ea2b32af4d908d16d72d3ec
Gregory <lkorron@yandex.ru>
2020-07-29 16:41:56 +0300

    Escape

---
+++ Hubba-Turbo New/Assets/Scripts/Escape.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Escape : MonoBehaviour
+{
+    [Range(0.0f, 1.0f)]
+    [SerializeField] private float flyingForce;
+
+    private Rigidbody2D m_rigidbody;
+    private void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            StartEscape();
+        }
+           
+    }
+
+    private void StartEscape()
+    {
+        // multiple for comfortable
+        m_rigidbody.gravityScale = flyingForce * -1;
+    }
+
+}
+
+public enum Fear
+{
+    mouse,
+    snake,
+    bird,
+    dog,
+    wolf
+}
