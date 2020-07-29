19f6ffba9476f8e6f57412d01e2565abdaaf45c5
Gregory <lkorron@yandex.ru>
2020-07-29 11:04:48 +0300

    Add second variant of sheep anim

---
+++ Hubba-Turbo New/Assets/Scripts/ItemAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemAnimation : MonoBehaviour
+{
+    [SerializeField] private float delay;
+    [SerializeField] private string animationName;
+    private Animator animator;
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine(DelayAnimation());
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator DelayAnimation()
+    {
+        yield return new WaitForSeconds(delay);
+        animator.Play(animationName);
+    }
+}
