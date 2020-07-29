524700e3f753fcc58699098c9ec5af30f5699cb2
Gregory <lkorron@yandex.ru>
2020-07-29 14:07:58 +0300

    animation with delay

--- Hubba-Turbo New/Assets/Scripts/ItemAnimation.cs
+++ Hubba-Turbo New/Assets/Scripts/ItemAnimation.cs
@@ -4,25 +4,34 @@ using UnityEngine;
 
 public class ItemAnimation : MonoBehaviour
 {
-    [SerializeField] private float delay;
-    [SerializeField] private string animationName;
+    [SerializeField] private float delay; // Delay between animations
+    [SerializeField] private AnimationClip m_animationClip;
+    [SerializeField] private bool isPlaying = true; // Play or not
+
     private Animator animator;
+    private float animationTime = 2.8f; // Time while animation playing
+    private string animationName;
     
     void Start()
     {
-        animator = GetComponent<Animator>();
-        StartCoroutine(DelayAnimation());
-    }
 
-    
-    void Update()
-    {
+        animator = GetComponent<Animator>();
+
+        animationName = m_animationClip.name;
+        StartCoroutine(DelayAnimation());
         
     }
 
+    
+
     private IEnumerator DelayAnimation()
     {
-        yield return new WaitForSeconds(delay);
-        animator.Play(animationName);
+        while (isPlaying)
+        {
+            yield return new WaitForSeconds(delay);
+            animator.Play(animationName);
+            yield return new WaitForSeconds(animationTime);
+        }
+        
     }
 }
