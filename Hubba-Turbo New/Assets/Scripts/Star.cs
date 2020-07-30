7faf06b57bc6e35c8da02a054d3de8323f718dde
Gregory <lkorron@yandex.ru>
2020-07-30 12:45:23 +0300

    Star script

---
+++ Hubba-Turbo New/Assets/Scripts/Star.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    // Index for 
+    [SerializeField] private int index;
+
+    public int GetIndex()
+    {
+        return index;
+    }
+}
