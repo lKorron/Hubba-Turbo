04162f8b1fee8bbacfc307ee893c0f1cb48b1d28
Gregory <lkorron@yandex.ru>
2020-07-31 12:54:16 +0300

    add array

---
+++ Hubba-Turbo New/Assets/Scripts/Data/Level.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] private Sprite levelSprite;
+    public int levelNumber;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+    public void SetOneStar()
+    {
+
+    }
+
+}
---
+++ Hubba-Turbo New/Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private Level[] levels;
+
+    private void Start()
+    {
+        levels = FindObjectsOfType<Level>();
+        SetLevelImages();
+    }
+
+    private void SetLevelImages()
+    {
+        print(levels[0].levelNumber);
+    }
+}
