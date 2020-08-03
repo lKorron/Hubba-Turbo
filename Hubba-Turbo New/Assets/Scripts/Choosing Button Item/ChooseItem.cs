6c1cbe42b3ac06939d41e44441eadc410050c05f
Gregory <lkorron@yandex.ru>
2020-08-03 16:39:43 +0300

    Start button item

---
+++ Hubba-Turbo New/Assets/Scripts/Choosing Button Item/ChooseItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseItem : MonoBehaviour
+{
+    private ButtonItem[] buttons;
+
+    private void Start()
+    {
+        buttons = FindObjectsOfType<ButtonItem>();
+    }
+    
+}
