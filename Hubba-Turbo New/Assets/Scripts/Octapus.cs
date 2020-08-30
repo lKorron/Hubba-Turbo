5ace4426171a2311b4a3cc1e14a3b84a235a164d
Gregory <lkorron@yandex.ru>
2020-08-30 13:21:10 +0300

    New script and new sprite

--- Hubba-Turbo New/Assets/Scripts/Octapus.cs
+++ Hubba-Turbo New/Assets/Scripts/Octapus.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class Octapus : MonoBehaviour
 {
     [SerializeField] private PhysicsMaterial2D lowFrictionMaterial;
-    private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite slipperyPlatformSprite;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Platform")
         {
-            BoxCollider2D collisionBoxCollider = collision.gameObject.GetComponent<BoxCollider2D>();
-            collisionBoxCollider.sharedMaterial = lowFrictionMaterial;
+            SetPlatformSlippery(collision);
         }
     }
 
-    private void SetPlatformSlippery()
+    private void SetPlatformSlippery(Collision2D collision)
     {
+        // Change friction
+        BoxCollider2D collisionBoxCollider = collision.gameObject.GetComponent<BoxCollider2D>();
+        collisionBoxCollider.sharedMaterial = lowFrictionMaterial;
 
+        // Change sprite
+        SpriteRenderer collisionSpriteRenderer = collision.gameObject.GetComponent<SpriteRenderer>();
+        collisionSpriteRenderer.sprite = slipperyPlatformSprite;
     }
 }
