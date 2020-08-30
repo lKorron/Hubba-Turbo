fb067adb9c45ed3d15b9e15cfd1cec61d52dd5a5
Gregory <lkorron@yandex.ru>
2020-08-30 12:06:36 +0300

    Start slippery

---
+++ Hubba-Turbo New/Assets/Scripts/Octapus.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Octapus : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            print("Platform");
+        }
+    }
+
+    private void SetPlatformSlippery()
+    {
+
+    }
+}
