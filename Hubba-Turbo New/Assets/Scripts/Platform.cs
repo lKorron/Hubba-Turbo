df67bcaec2af0c401f778b1a5641abc30016fccb
Gregory <lkorron@yandex.ru>
2020-08-30 13:45:04 +0300

    Clear platform script

--- Hubba-Turbo New/Assets/Scripts/Platform.cs
+++ Hubba-Turbo New/Assets/Scripts/Platform.cs
@@ -5,62 +5,7 @@ using UnityEngine.Events;
 
 public class Platform : MonoBehaviour
 {
-    [SerializeField] private bool IsComputerActive = true; // Field determines will computer get turn or not
-    [SerializeField] private UnityEvent OnCollision; // Event for computer instantiate
-    [SerializeField] private ObjectInstantiate objectInstantiate; // Instance for check computer activity
-    private WeightComparing weightComparing; // Instance for comparing
     
-
-    private void Start()
-    {
-        weightComparing = GetComponent<WeightComparing>();
-    }
-
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-       
-        // Processing collision with tag
-        switch (collision.gameObject.tag)
-        { 
-            case "PlayerStart":
-                SetItem(collision, Side.Player);
-                break;
-            case "Player":
-                SetItem(collision, Side.Player);
-                if (IsComputerActive)
-                {
-                    OnCollision.Invoke(); // Computer turn
-                }
-                else weightComparing.Compare();
-                
-                break;
-            case "Computer":
-                SetItem(collision, Side.Computer);
-                weightComparing.Compare();
-                break;
-            
-        }
-        // Comparing
-        if (objectInstantiate.IsComputerEndInstantiate)
-        {
-            weightComparing.Compare();
-        }
-    }
-
-    // Adding item (collision) to instance array
-    private void SetItem(Collision2D collision, Side side)
-    {
-        collision.gameObject.tag = "Untagged";
-     
-        Item item = collision.gameObject.GetComponent<Item>();
-        if (item != null)
-        {
-            weightComparing.AddItem(item, side);
-            
-        }
-        else print("item is null");
-    }
 
     
 
