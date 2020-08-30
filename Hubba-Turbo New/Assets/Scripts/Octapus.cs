90cab3d95b3b054368bae2bfebe8e9d071eed7ba
Gregory <lkorron@yandex.ru>
2020-08-30 13:59:25 +0300

    End slippery platform

--- Hubba-Turbo New/Assets/Scripts/Octapus.cs
+++ Hubba-Turbo New/Assets/Scripts/Octapus.cs
@@ -4,33 +4,19 @@ using UnityEngine;
 
 public class Octapus : MonoBehaviour
 {
-    private PhysicsMaterial2D lowFrictionMaterial;
-    private Sprite slipperyPlatformSprite;
 
-    private bool isPlatformSlippery = false;
+    private Platform platform;
+
+    private void Start()
+    {
+        platform = FindObjectOfType<Platform>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Platform" && isPlatformSlippery == false)
+        if (collision.gameObject.tag == "Platform" && platform.IsPlatformSlippery == false)
         {
-            SetPlatformSlippery(collision);
+            platform.SetPlatformSlippery();
         }
-    }
-
-    private void SetPlatformSlippery(Collision2D collision)
-    {
-        print("Start method");
-        isPlatformSlippery = true;
-
-        lowFrictionMaterial = Resources.Load<PhysicsMaterial2D>("Materials/LowFriction");
-        slipperyPlatformSprite = Resources.Load<Sprite>("Sprites/Platforms/PlatformSlippery");
-
-        // Change friction
-        BoxCollider2D collisionBoxCollider = collision.gameObject.GetComponent<BoxCollider2D>();
-        collisionBoxCollider.sharedMaterial = lowFrictionMaterial;
-
-        // Change sprite
-        SpriteRenderer collisionSpriteRenderer = collision.gameObject.GetComponent<SpriteRenderer>();
-        collisionSpriteRenderer.sprite = slipperyPlatformSprite;
     }
 }
--- Hubba-Turbo New/Assets/Scripts/Platform.cs
+++ Hubba-Turbo New/Assets/Scripts/Platform.cs
@@ -5,9 +5,31 @@ using UnityEngine.Events;
 
 public class Platform : MonoBehaviour
 {
-    
+    private PhysicsMaterial2D lowFrictionMaterial;
+    private Sprite slipperyPlatformSprite;
 
-    
+    public bool IsPlatformSlippery { get; private set; }
 
-    
+    private void Start()
+    {
+        lowFrictionMaterial = Resources.Load<PhysicsMaterial2D>("Materials/LowFriction");
+        slipperyPlatformSprite = Resources.Load<Sprite>("Sprites/Platforms/PlatformSlippery");
+    }
+
+    public void SetPlatformSlippery()
+    {
+        IsPlatformSlippery = true;
+
+        // Change friction
+        BoxCollider2D collisionBoxCollider = GetComponent<BoxCollider2D>();
+        collisionBoxCollider.sharedMaterial = lowFrictionMaterial;
+
+        // Change sprite
+        SpriteRenderer collisionSpriteRenderer = GetComponent<SpriteRenderer>();
+        collisionSpriteRenderer.sprite = slipperyPlatformSprite;
+    }
+
+
+
+
 }
