23970cd23a623dfadb8ed2a43a3fdeac3b272639
Gregory <lkorron@yandex.ru>
2020-08-30 13:41:29 +0300

    Changes

--- Hubba-Turbo New/Assets/Scripts/Octapus.cs
+++ Hubba-Turbo New/Assets/Scripts/Octapus.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Octapus : MonoBehaviour
 {
-    [SerializeField] private PhysicsMaterial2D lowFrictionMaterial;
-    [SerializeField] private Sprite slipperyPlatformSprite;
+    private PhysicsMaterial2D lowFrictionMaterial;
+    private Sprite slipperyPlatformSprite;
+
+    private bool isPlatformSlippery = false;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Platform")
+        if (collision.gameObject.tag == "Platform" && isPlatformSlippery == false)
         {
             SetPlatformSlippery(collision);
         }
@@ -17,6 +19,12 @@ public class Octapus : MonoBehaviour
 
     private void SetPlatformSlippery(Collision2D collision)
     {
+        print("Start method");
+        isPlatformSlippery = true;
+
+        lowFrictionMaterial = Resources.Load<PhysicsMaterial2D>("Materials/LowFriction");
+        slipperyPlatformSprite = Resources.Load<Sprite>("Sprites/Platforms/PlatformSlippery");
+
         // Change friction
         BoxCollider2D collisionBoxCollider = collision.gameObject.GetComponent<BoxCollider2D>();
         collisionBoxCollider.sharedMaterial = lowFrictionMaterial;
