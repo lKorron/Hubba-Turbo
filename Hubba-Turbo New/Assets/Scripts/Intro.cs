76980461c4deeaf770acee297aa35d263415ca2f
Gregory <lkorron@yandex.ru>
2020-09-02 18:18:38 +0300

    Update Intro.cs

--- Hubba-Turbo New/Assets/Scripts/Intro.cs
+++ Hubba-Turbo New/Assets/Scripts/Intro.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class Intro : MonoBehaviour
 {
     [SerializeField] private float waitingTime;
+    [SerializeField] private int MainMenuIndex;
 
     private void Start()
     {
@@ -15,7 +16,7 @@ public class Intro : MonoBehaviour
     private IEnumerator WaitForVideo()
     {
         yield return new  WaitForSeconds(waitingTime);
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(MainMenuIndex);
         
     }
 }
