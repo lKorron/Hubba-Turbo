8e29ca6497b786e5d01daf19da1bad47116e0073
Gregory <lkorron@yandex.ru>
2020-09-02 17:53:32 +0300

    Add intro script and scene

---
+++ Hubba-Turbo New/Assets/Scripts/Intro.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour
+{
+    [SerializeField] private float waitingTime = 5f;
+
+    private void Start()
+    {
+        StartCoroutine(WaitForVideo());
+    }
+
+    private IEnumerator WaitForVideo()
+    {
+        yield return new  WaitForSeconds(waitingTime);
+        
+    }
+}
