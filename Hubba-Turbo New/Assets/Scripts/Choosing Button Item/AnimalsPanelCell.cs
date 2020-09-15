749ae76bcbd9d2d69b0b2f5017b63c0d30d3f912
Gregory <lkorron@yandex.ru>
2020-09-15 12:37:09 +0300

    Render panel

--- Hubba-Turbo New/Assets/Scripts/Choosing Button Item/AnimalsPanelCell.cs
+++ Hubba-Turbo New/Assets/Scripts/Choosing Button Item/AnimalsPanelCell.cs
@@ -6,10 +6,23 @@ using UnityEngine.UI;
 public class AnimalsPanelCell : MonoBehaviour
 {
     [SerializeField] private Image _image;
+    private Toggle _toggle;
+    private ToggleGroup _toggleGroup;
 
+    private void Start()
+    {
+        _toggle = GetComponent<Toggle>();
+        _toggleGroup = GetComponentInParent<ToggleGroup>();
+
+        _toggle.group = _toggleGroup;
+    }
     public void Render(AnimalItem animalItem)
     {
         _image.sprite = animalItem.IconSprite;
+    }
+    public void SetOn()
+    {
+        _toggle.isOn = true;
     }
 
 }
