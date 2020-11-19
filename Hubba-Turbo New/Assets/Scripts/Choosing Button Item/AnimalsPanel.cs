193032429278bc0171759131c6ccbc1048a6d978
Gregory <lkorron@yandex.ru>
2020-11-19 12:28:40 +0300

    Revert "Update AnimalsPanel.cs"
    This reverts commit cb380dbc82b402a6facc3aa0b866c1ab283eae47.

--- Hubba-Turbo New/Assets/Scripts/Choosing Button Item/AnimalsPanel.cs
+++ Hubba-Turbo New/Assets/Scripts/Choosing Button Item/AnimalsPanel.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class AnimalsPanel : MonoBehaviour
 {
@@ -16,7 +15,6 @@ public class AnimalsPanel : MonoBehaviour
     {
         _instantiateSettings = InstantiateSettings.Instance;
         _animalItems = _instantiateSettings.AnimalItems;
-        ChangeColor();
 
         Render(_animalItems);
         
@@ -28,14 +26,6 @@ public class AnimalsPanel : MonoBehaviour
         {
             var cell = Instantiate(_animalsPanelCellTemplate, _container);
             cell.Render(item);
-         
         }
-    }
-
-    private void ChangeColor()
-    {
-        var button = _animalsPanelCellTemplate.gameObject.GetComponent<Button>();
-        Color color = button.colors.highlightedColor;
-        color = Color.yellow;
     }
 }
