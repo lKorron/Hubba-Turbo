0e79e8079c308a54efea34f1890f92ca0918ae71
Gregory <lkorron@yandex.ru>
2020-11-21 16:11:04 +0300

    Mine part 2

---
+++ Hubba-Turbo New/Assets/Scripts/Mechanics/Mine/Explosion.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PointEffector2D))]
+public class Explosion : MonoBehaviour
+{
+    [SerializeField] private float _explosionForce;
+    [SerializeField] private float _explosionForceVariation;
+    [SerializeField] private float _destroyingTime;
+
+    private PointEffector2D _pointEffector;
+
+    #region OnValidate
+    private void OnValidate()
+    {
+        if (_explosionForce < 0)
+            _explosionForce = 0;
+
+        if (_explosionForceVariation < 0)
+            _explosionForceVariation = 0;
+
+        if (_destroyingTime < 0)
+            _destroyingTime = 0;
+    }
+    #endregion
+    private void OnEnable()
+    {
+        _pointEffector = GetComponent<PointEffector2D>();
+        _pointEffector.forceMagnitude = _explosionForce;
+        _pointEffector.forceVariation = _explosionForceVariation;
+
+        Destroy(gameObject, _destroyingTime);
+    }
+}
