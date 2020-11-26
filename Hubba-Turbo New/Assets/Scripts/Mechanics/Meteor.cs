cb36fb3159298b2f7880280aa9cdbec0cc7970f3
Gregory <lkorron@yandex.ru>
2020-11-26 17:06:04 +0300

    MeteorMovement

---
+++ Hubba-Turbo New/Assets/Scripts/Mechanics/Meteor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteor : MonoBehaviour
+{
+    [SerializeField] private float _radius;
+    [SerializeField] private float _speed;
+    [SerializeField] private Vector2 _centerPoint;
+
+    private float _moveValue;
+
+    private void Update()
+    {
+        Fly();
+    }
+
+    private void Fly()
+    {
+        _moveValue += Time.deltaTime * _speed;
+
+        float xPosition = _radius * Mathf.Cos(_moveValue) + _centerPoint.x;
+        float yPosition = _radius * Mathf.Sin(_moveValue) + _centerPoint.y;
+
+        transform.position = new Vector3(xPosition, yPosition, 0);
+    }
+}
