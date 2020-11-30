0b4272ba637dd96264a1ffa5a31733b8d8dbdba5
Gregory <lkorron@yandex.ru>
2020-11-30 17:59:03 +0300

    New movements

--- Hubba-Turbo New/Assets/Scripts/Mechanics/CyclicalMovement/MovementTypes/InclinedEightMovement.cs
+++ Hubba-Turbo New/Assets/Scripts/Mechanics/CyclicalMovement/MovementTypes/InclinedEightMovement.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LeftCircleMovement : AbstractСyclicalMovement
+public class InclinedEightMovement : AbstractСyclicalMovement
 {
     public override void Move(GameObject gameObject, float speed, float radius, Vector2 centerPoint)
     {
         _moveValue += Time.deltaTime * speed;
 
-        float xPosition = radius * Mathf.Cos(_moveValue ) + centerPoint.x;
-        float yPosition = radius * Mathf.Sin(_moveValue) + centerPoint.y;
+        float xPosition = radius * Mathf.Cos(_moveValue) + centerPoint.x;
+        float yPosition = radius * Mathf.Sin(_moveValue * 2) + centerPoint.y;
 
         gameObject.transform.position = new Vector3(xPosition, yPosition, 0);
     }
