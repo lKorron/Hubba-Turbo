87e8fce291e2e77b2e0caeefc7af55bad8c5a27c
Gregory <lkorron@yandex.ru>
2020-12-14 16:10:47 +0300

    Time snail core

---
+++ Hubba-Turbo New/Assets/Scripts/Mechanics/TimeSnail/TimeChange.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ItemCollision))]
+public class TimeChange : MonoBehaviour
+{
+    [SerializeField] private float _acceleratedTimeScale;
+    [SerializeField] private float _slowedTimeScale;
+
+    private ItemCollision _itemCollision;
+    private Side _currentSide;
+
+    public event Action SideChanged;
+
+    public Side CurrentSide => _currentSide;
+
+    #region OnValidate
+    private void OnValidate()
+    {
+        if (_acceleratedTimeScale < 1)
+            _acceleratedTimeScale = 1;
+
+        if (_slowedTimeScale > 1 || _slowedTimeScale < 0)
+            _slowedTimeScale = 0;
+    }
+    #endregion
+
+    private void Start()
+    {
+        _itemCollision = GetComponent<ItemCollision>();
+    }
+
+    private void Update()
+    {
+        var side = _itemCollision.Side;
+
+        if (side != _currentSide)
+        {
+            _currentSide = side;
+            SideChanged.Invoke();
+        }
+
+        if (_currentSide == Side.Player)
+        {
+            SetTimeScale(_acceleratedTimeScale);
+        }
+
+        if (_currentSide == Side.Computer)
+        {
+            SetTimeScale(_slowedTimeScale);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        SetTimeScale(1f);
+    }
+
+    private void SetTimeScale(float scale)
+    {
+        Time.timeScale = scale;
+    }
+}
---
+++ Hubba-Turbo New/Assets/Scripts/Mechanics/TimeSnail/TimeSnailPresenter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(TimeChange))]
+public class TimeSnailPresenter : MonoBehaviour
+{
+    [SerializeField] private Sprite _slowingNailSprite;
+    [SerializeField] private Sprite _acceleratingNailSprite;
+
+    private SpriteRenderer _currentSprite;
+    private TimeChange _timeChange;
+
+    private void Start()
+    {
+        _currentSprite = GetComponent<SpriteRenderer>();
+        _timeChange = GetComponent<TimeChange>();
+
+        _timeChange.SideChanged += () => SetSprite();
+    }
+
+    private void OnDisable()
+    {
+        _timeChange.SideChanged -= () => SetSprite();
+    }
+
+    private void SetSprite()
+    {
+        
+    }
+}
